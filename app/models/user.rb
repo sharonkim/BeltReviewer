b791ac2ce1f55ca218acd56e7c0165be22cd3dc6
Sharon Kim <sharon.kim0@gmail.com>
2017-04-24 14:54:28 -0700

    app created for deployment

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 	has_many :events, through: :user_events
 
 	validates :first_name, :last_name, :email, :city, :state, presence: true
-	validates :email, uniqueness: true { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i }
+	validates :email, uniqueness: { case_sensitive: false }, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i }
 	validates :state, length: { is:2 }
 	validates :password, length: { minimum:8 }, on: :create
 	validates :password_confirmation
